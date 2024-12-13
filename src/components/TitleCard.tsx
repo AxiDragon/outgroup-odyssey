e26c3d04ef62cf780eac066b5fe5c65ff5902cbb
Axi <axidreamdragon@gmail.com>
2024-12-13 11:50:23 +0100

    Update basic TitleCard

--- src/components/TitleCard.tsx
+++ src/components/TitleCard.tsx
@@ -1,8 +1,9 @@
 const TitleCard = () => {
 	return (
-		<div>
+		<div className="TitleCard">
 			<h1>OUTGROUP ODYSSEY</h1>
 			<p>An interactive article about outgroup homogeneity bias!</p>
+			<p><sub>Please note that this is still very much a work-in-progress...</sub></p>
 		</div>
 	);
 };
