426cbb66f3bceb4fcb04e3554c89408ca5a01efc
Axi <axidreamdragon@gmail.com>
2024-12-13 16:25:11 +0100

    Implement solution slide show

--- src/components/sections/SolutionSection.tsx
+++ src/components/sections/SolutionSection.tsx
@@ -1,3 +1,11 @@
+import ImageSlideShow from "@c/interactables/ImageSlideShow";
+
+import solution1 from "@a/placeholder/solution-1.jpg";
+import solution2 from "@a/placeholder/solution-2.jpg";
+import solution3 from "@a/placeholder/solution-3.jpg";
+import solution4 from "@a/placeholder/solution-4.jpg";
+import solution5 from "@a/placeholder/solution-5.jpg";
+
 const SolutionSection = () => {
 	return (
 		<div>
@@ -8,9 +16,10 @@ const SolutionSection = () => {
 				One thing that helps with that is simply being conscious about its existence.
 				Which, you should be, now that you've gone through this article. Go you!</p>
 			<p>Once you do realize that you're generalizing an outgroup, though - what do you do then?</p>
-			<p><sub>Graphic where you can click on the image, progressing slowly making the resulting group.</sub></p>
+			<ImageSlideShow images={[solution1, solution2, solution3, solution4, solution5]} />
+			<p style={{ fontSize: 10 }}><i>Tap the image!</i></p>
 			<p>...try to make the mental ingroup you're in, contain the outgroup too!</p>
-			<p><i>I should put a better ending here. but I'm not sure what exactly.</i></p>
+			<p><i>I should put a better ending here. but I'm not sure what exactly. give me ideas please</i></p>
 		</div>
 	);
 }
