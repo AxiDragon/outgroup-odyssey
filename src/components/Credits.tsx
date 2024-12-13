1d7a6da8ef2450f9031307c8137dda1510d04b94
Axi <axidreamdragon@gmail.com>
2024-12-13 16:33:36 +0100

    Add credits

--- src/components/Credits.tsx
+++ src/components/Credits.tsx
@@ -1,9 +1,13 @@
 const Credits = () => {
 	return (
-		<div>
-			<p>Thanks to ME! HAHAHHA</p>
-			<p>I should put sources here.</p>
-			<p>But also definitely thanks to Nicky Case for the inspiration.</p>
+		<div className="Credits">
+			<p>Thank you for playing this explorable explanation!</p>
+			<h4>References</h4>
+			<ul>
+				<li>Sapolsky, Robert M. (2017). Behave: The Biology of Humans at Our Best and Worst. Penguin Press.</li>
+				<li>Ackerman, Joshua M., Jenessa R. Shapiro, Steven L. Neuberg, Douglas T. Kenrick, D. Vaughn Becker, Vladas Griskevicius, Jon K. Maner & Mark Schaller (2006). They all look the same to me (Unless they're angry): From out-group homogeneity to out-group heterogeneity. Psychological Science 17(10): 836-40.</li>
+				<li>Nicky Case (for inspiration!)</li>
+			</ul>
 		</div>
 	);
 }
