759657359135d75481d4a2d6548b0e55e766c092
Axi <axidreamdragon@gmail.com>
2025-01-03 17:00:29 +0100

    Fix grammar and clarity issues in CauseSection text

--- src/components/sections/CauseSection.tsx
+++ src/components/sections/CauseSection.tsx
@@ -5,11 +5,11 @@ const CauseSection = () => {
 	return (
 		<div>
 			<h2>RIGHT, THAT MAKES SENSE. BUT WHY?</h2>
-			<p>One potential reason is simply that we don't interact much with outgroups,
+			<p>One might think that outgroup homogeneity bias exists simply don't interact much with outgroups,
 				and therefore don't know people from outgroups as well.
-				However, this is disputed since outgroup homogeneity bias still occurs in groups that
+				However, it is unsure if this is the main cause of the bias, as it still occurs in groups that
 				frequently interact with each other, like men and women.<Footnote referenceId={2} /></p>
-			<p>Another reason that has more consensus is that it's far easier on the brain to
+			<p>A reason that has more consensus is that it's far easier on the brain to
 				think that an outgroup consists	of all the same sorts of people.
 				You don't have to remember as many things.<Footnote referenceId={3} /></p>
 			<p>Try messing with the tool below!</p>
