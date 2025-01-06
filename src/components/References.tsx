ebe299e2c552670bada3f055818a999e5b7904e5
Axi <axidreamdragon@gmail.com>
2025-01-06 15:33:05 +0100

    Update colors

--- src/components/References.tsx
+++ src/components/References.tsx
@@ -3,9 +3,11 @@ type Props = {
 }
 
 const References = ({ sources }: Props) => {
-	return (<div className="References">
-		<h4>Sources</h4>
-		{sources.map((source, i) => <div key={i} id={`reference${i + 1}`}>[{i + 1}] {source}</div>)}
+	return (<div className="References Section">
+		<h2>Sources</h2>
+		<div className="SectionText">
+			{sources.map((source, i) => <div key={i} id={`reference${i + 1}`}>[{i + 1}] {source}</div>)}
+		</div>
 	</div>);
 }
 
